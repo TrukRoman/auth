e2617334f4a32b88cf531e6ffd4abcf9fb6f06c5
Roman Trukhanovich <trukroman@gmail.com>
2023-10-11 15:04:07 +0300

    add validation for update user request

--- src/main/java/com/example/auth/dto/UpdateUserRequest.java
+++ src/main/java/com/example/auth/dto/UpdateUserRequest.java
@@ -1,9 +1,13 @@
 package com.example.auth.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
 import java.util.List;
 
-public record UpdateUserRequest(Long id,
-                                String firstName,
-                                String lastName,
-                                List<String> phoneNumbers) {
+public record UpdateUserRequest(@NotNull Long id,
+                                @Length(min = 1, max = 100) String firstName,
+                                @Length(min = 1, max = 100) String lastName,
+                                @Size(max = 20) List<String> phoneNumbers) {
 }
